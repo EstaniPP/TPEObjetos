37b51857bcb36656a091243a46246ba5e97e7c04
Estanislao Perez Pe <Estanislao Perez Pe@LAPTOP-559QH9HJ>
2018-08-11 18:45:12 -0300

    prueba

--- Stocker/src/Main.java
+++ Stocker/src/Main.java
@@ -5,14 +5,13 @@ import java.util.Vector;
 public class Main {
 	public static void main(String[] args) throws SQLException {
 		DBManager db = new DBManager();	
-		
+
 		FiltroArticulo.idInterno f1 = new FiltroArticulo.idInterno(2);
 		FiltroArticulo.descripcion f2 = new FiltroArticulo.descripcion("des");
 		Vector<Articulo> va = db.getArticulos(f2);
 		for(Articulo a : va) {
 			System.out.println(a.getCodigoBarras());
 		}
-		
 		System.out.println("EXITO");
 	}
 }
