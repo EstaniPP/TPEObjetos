bfc67b5317a41074bf47aa0c61c2b125d7280942
Estanislao Perez Pe <Estanislao Perez Pe@LAPTOP-559QH9HJ>
2018-08-11 17:44:04 -0300

    Clase Venta terminada, falta resolver el tema del Id

---
+++ Stocker/src/Venta.java
@@ -0,0 +1,56 @@
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public abstract class Venta {
+	// id del auto increment de la base de datos
+	int idVenta;
+	// tipo de venta que se realizo (con o sin cliente)
+	int tipoVenta;
+	// fecha en que se realizo la venta
+	Date fechaVenta;
+	//Productos que se vendieron
+	ArrayList<Articulo> articulos;
+	//Hash con id de producto y cantidad.
+	HashMap<Integer, Integer> cantidadArticulos;
+	//id del cliente que realizo la compra (si es '0' es una compra sin cliente)
+	int idClienteVenta;
+	
+	public Venta(int tipoVenta, Date fechaventa, int idClienteVenta) {
+		this.tipoVenta = tipoVenta;
+		this.fechaVenta = fechaventa;
+		this.idClienteVenta = idClienteVenta;
+	}
+	
+	public abstract float getPrecioAPagar();
+	
+	public float getPrecioTotal() {
+		float total = 0;
+		for(int i=0;i<articulos.size();i++) {
+			total += articulos.get(i).getPrecioUnitario()*cantidadArticulos.get(articulos.get(i).getIdInterno());
+		}
+		return total;
+	}
+	
+	public boolean agregarArticulo(Articulo art, Integer cant) {
+		if(articulos.contains(art)) {
+			cantidadArticulos.put(art.getIdInterno(), cant+cantidadArticulos.get(art.idInterno));
+		}else {
+			articulos.add(art);
+			cantidadArticulos.put(art.getIdInterno(), cant);
+		}
+		return true;
+	}
+	
+	public boolean borrarArticulo(Articulo art) {
+		if(articulos.contains(art)) {
+			articulos.remove(art);
+			cantidadArticulos.remove(art.getIdInterno());
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	
+}
