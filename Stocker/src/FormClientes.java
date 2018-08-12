ad477ae88630eeed5ef71673bad5758040864f89
Valentin <valentinfvazquez@gmail.com>
2018-08-12 02:02:47 -0300

    Modificacion formulario de clientes

--- Stocker/src/FormClientes.java
+++ Stocker/src/FormClientes.java
@@ -13,10 +13,17 @@ import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.JTable;
+import javax.swing.JSeparator;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.sql.SQLException;
+import java.util.Vector;
 
 public class FormClientes extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 	private JTable table;
 
 	/**
@@ -27,6 +34,10 @@ public class FormClientes extends JFrame {
 	 * Create the frame.
 	 */
 	public FormClientes() {
+		
+		// llamado a bd
+		DBManager db = new DBManager();
+		// llamado a bd
 		setTitle("CLIENTES");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 663, 340);
@@ -39,17 +50,55 @@ public class FormClientes extends JFrame {
 		lblBuscarClientes.setBounds(6, 6, 121, 16);
 		contentPane.add(lblBuscarClientes);
 		
+		textField = new JTextField();
+		textField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == 10) {
+					// se presiono enter
+					Vector<Cliente> vector = new Vector<Cliente>();
+					try {
+						vector = db.getClientes(null);
+					}catch(SQLException e1) {
+						e1.getStackTrace();
+					}
+					System.out.println(vector.size());
+				}
+			}
+		});
+		textField.setToolTipText("NOMBRE");
+		textField.setBounds(6, 51, 217, 30);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(6, 24, 525, 12);
+		contentPane.add(separator);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(6, 38, 61, 16);
+		contentPane.add(lblNombre);
+		
+		JLabel lblIdentificador = new JLabel("ID");
+		lblIdentificador.setBounds(224, 38, 156, 16);
+		contentPane.add(lblIdentificador);
+		
+		textField_1 = new JTextField();
+		textField_1.setToolTipText("NOMBRE");
+		textField_1.setColumns(10);
+		textField_1.setBounds(224, 51, 217, 30);
+		contentPane.add(textField_1);
+		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setBounds(6, 97, 467, 200);
+		scrollPane.setBounds(10, 81, 428, 200);
 		contentPane.add(scrollPane);
-		 String[] colName = new String[] { "Product Name" ,"Price" };
-	        Object[][] products = new Object[][] { 
-	                { "Galleta" ,"$80" },
-	                { "Malta" ,"$40" }
-	               
-	            };
-		table = new JTable(products, colName);
+		
+		String[] columnas = new String[] {"ID", "NOMBRE", "TELEFONO", "EMAIL", "TIPO"};
+		Object[][] clientes = new Object[][] {};
+		table = new JTable(clientes, columnas);
+		table.setCellSelectionEnabled(true);
 		scrollPane.setViewportView(table);
+		
 	}
 }
