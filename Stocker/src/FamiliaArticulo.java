f16c53800080944db15cb1178add54581d01fcc4
Estanislao Perez Pe <Estanislao Perez Pe@LAPTOP-559QH9HJ>
2018-08-13 18:03:41 -0300

    FamiliaArticulo

---
+++ Stocker/src/FamiliaArticulo.java
@@ -0,0 +1,18 @@
+
+public class FamiliaArticulo {
+	int idFamilia;
+	String nombreFamilia;
+	
+	public FamiliaArticulo(int idFamilia, String nombreFamilia) {
+		this.idFamilia = idFamilia;
+		this.nombreFamilia = nombreFamilia;
+	}
+	
+	public int getIdFamilia() {
+		return idFamilia;
+	}
+	
+	public String getNombreFamilia() {
+		return nombreFamilia;
+	}	
+}
