6e89fe28b9f8332ef6267ced03490e76da4d741b
Valentin <valentinfvazquez@gmail.com>
2018-08-14 12:56:52 -0300

    -

--- Stocker/src/Main.java
+++ Stocker/src/Main.java
@@ -2,6 +2,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
 
+
 import Articulos.Articulo;
 import DataBase.DBManager;
 import Filtros.FiltroArticulo;
@@ -9,15 +10,6 @@ import Filtros.FiltroCompuesto;
 
 public class Main {
 	public static void main(String[] args) throws SQLException {
-		DBManager db = new DBManager();	
-
-		FiltroArticulo.descripcion f2 = new FiltroArticulo.descripcion("FSDFDF");
-		FiltroArticulo.descripcion f3 = new FiltroArticulo.descripcion("khkjkgj");
-		FiltroCompuesto.Or f4 = new FiltroCompuesto.Or(f2, f3);
-		Vector<Articulo> va = db.getArticulos(f4);
-		for(Articulo a : va) {
-			System.out.println(a.getDescripcion());
-		}
-		System.out.println(va.size());
+		//Robot robot = new Robot();		
 	}
 }
