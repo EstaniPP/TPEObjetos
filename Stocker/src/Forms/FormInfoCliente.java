aff868be32411cb4483dd7f1a28ffdcfc5bd40dd
Estanislao Perez Pe <Estanislao Perez Pe@LAPTOP-559QH9HJ>
2018-08-20 19:55:03 -0300

    redy2

--- Stocker/src/Forms/FormInfoCliente.java
+++ Stocker/src/Forms/FormInfoCliente.java
@@ -20,7 +20,7 @@ public class FormInfoCliente extends JFrame {
 	 */
 	public FormInfoCliente(Cliente c) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 771, 485);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
