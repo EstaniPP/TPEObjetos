1cd8ba145f0138cd91deafdf404150b7bfa2d986
Valentin <valentinfvazquez@gmail.com>
2018-08-21 17:26:38 -0300

    -

--- Stocker/src/Forms/FormNuevaVenta.java
+++ Stocker/src/Forms/FormNuevaVenta.java
@@ -5,9 +5,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
 
 import com.sun.glass.events.KeyEvent;
+import com.sun.glass.events.WindowEvent;
+import com.sun.xml.internal.ws.api.Component;
 
 import Articulos.Articulo;
 import DataBase.DBManager;
@@ -27,57 +31,34 @@ import javax.swing.JCheckBox;
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowListener;
 import java.sql.SQLException;
+import java.util.Vector;
 import java.awt.Color;
+import java.awt.event.ActionListener;
 
 public class FormNuevaVenta extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
 	DefaultTableModel model;
-	
+	Articulo articulo;
 	DBManager db = new DBManager();
+	private JTextField txtcod;
+	private JTextField txtcant;
 	
 	public static void main(String[] args) {
-		(new FormNuevaVenta()).setVisible(true);
+		FormNuevaVenta fnv = new FormNuevaVenta();
+		fnv.setVisible(true);
 	}
 	
-	public FormNuevaVenta() {
-		// hago que algunas columnas no sean editables!
-		model = new DefaultTableModel() {
-			public boolean isCellEditable(int row, int column) {
-				return column == 0 || column == 1;
-			}
-		};
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 797, 389);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		
-		
-		JLabel lblNuevaVenta = new JLabel("Nueva venta");
-		lblNuevaVenta.setFont(new Font("Lucida Grande", Font.BOLD, 13));
-		lblNuevaVenta.setBounds(16, 6, 174, 16);
-		contentPane.add(lblNuevaVenta);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		
-		scrollPane.setBounds(16, 34, 599, 303);
-		contentPane.add(scrollPane);
-		
-		table = new JTable();
-		table.setModel(model);
-		
-		
+	public void agregarEstilo(DefaultTableModel model) {
 		model.addColumn("COD. BARRAS");
 		model.addColumn("CANTIDAD");
 		model.addColumn("ARTICULO");
@@ -90,11 +71,42 @@ public class FormNuevaVenta extends JFrame {
 		
 		table.getColumnModel().getColumn(0).setPreferredWidth(75);
 		table.getColumnModel().getColumn(2).setPreferredWidth(200);
+	}
+	public FormNuevaVenta() {
+		// hago que algunas columnas no sean editables!
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 797, 389);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
+		model = new DefaultTableModel() {
+			public boolean isCellEditable(int row, int column) {
+				return column == 1;
+			}
+		};
+		
+		JLabel lblNuevaVenta = new JLabel("Nueva venta");
+		lblNuevaVenta.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		lblNuevaVenta.setBounds(16, 6, 174, 16);
+		contentPane.add(lblNuevaVenta);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		scrollPane.setBounds(16, 86, 599, 251);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(model);
+		
+		
+		agregarEstilo(model);
 		
 		table.setModel(model);
 		
-		//model.addRow(Object[] {"1", "2" ,"3" ,"4", "5"});
-		model.addRow(new Object[] {"443", "" ,"" ,"", "", "HOLA"});
 		
 		
 		// agrego tabla al jscrollpanel
@@ -114,72 +126,100 @@ public class FormNuevaVenta extends JFrame {
 		contentPane.add(lblNewLabel);
 		
 		JButton btnRealizarFactura = new JButton("REALIZAR FACTURA");
+		btnRealizarFactura.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		btnRealizarFactura.setForeground(new Color(34, 139, 34));
 		btnRealizarFactura.setBounds(627, 281, 150, 56);
 		contentPane.add(btnRealizarFactura);
 		
-		
-		table.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "enter");
-		table.getActionMap().put("enter", new AbstractAction() {
+		JLabel label = new JLabel("COD. BARRAS");
+		label.setBounds(19, 34, 85, 16);
+		contentPane.add(label);
+
+		txtcant = new JTextField();
+		txtcant.addKeyListener(new KeyAdapter() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				// aspectos a tener en cuenta.
-				/*
-				 * si esta en col 0 y presiona enter pasamos a unidad
-				 * llevar una variable con la columna ultima editada
-				 * si esta en columna 1 y presiona enter se crea una row nueva
-				 * inicializada en vacio y se setea el focus en la columna 0
-				 * con row = variable + 1 y columna 0
-				 * 
-				 * al presionar enter en cod de barras o columna 0
-				 * se hace un fetching de bd con el codigo de articulo
-				 * y se escribe en la tabla
-				 * si no hay codigo de barras (valido) por mas de que se presione enter en
-				 * cantidad no va a pasar nada
-				 * 
-				 * se tiene q poder ir al formulario de productos y seleccionar un producto de ahi
-				 * */
-				// paro la edicion in order to get the updated barcode from cell
-				if(table.isEditing()) {
-					table.getCellEditor().stopCellEditing();
+			public void keyPressed(java.awt.event.KeyEvent e) {
+				if(e.getKeyCode() == 10) {
+					FormNuevaVenta.this.model.addRow(new Object[] {
+							articulo.getCodigoBarras(),
+							txtcant.getText(),
+							articulo.getDescripcion(),
+							articulo.getPrecioUnitario(),
+							Double.valueOf(txtcant.getText()) * articulo.getPrecioUnitario(),
+							articulo
+					});
+					
+					txtcod.requestFocus();
+					txtcod.setText("");
+					txtcant.setText("");
 				}
-				//caso codigo de barras
-				if(table.getSelectedColumn() == 0) {
-					
-					// get producto de BD
-					String barcode = (String)table.getValueAt(table.getSelectedRow(), 0);
-					FiltroArticulo.codigoBarras fArt = new FiltroArticulo.codigoBarras(barcode);
-					Articulo art = Articulo.getArticuloError();
+			}
+		});
+		txtcant.setColumns(10);
+		txtcant.setBounds(122, 47, 103, 26);
+		contentPane.add(txtcant);
+		
+		
+		
+		
+		txtcod = new JTextField();
+		
+		txtcod.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(java.awt.event.KeyEvent e) {
+				if(e.getKeyCode() == 10) {
+					// chequeo si existe
+					articulo = Articulo.getArticuloError();
+					Vector<Articulo> vect = new Vector<Articulo>();
 					try {
-						art = db.getArticulos(fArt).elementAt(0);
-					}catch(SQLException e1) {
-						e1.printStackTrace();
+						vect = db.getArticulos(new FiltroArticulo.codigoBarras(txtcod.getText()));
+					}catch(SQLException esql1) {
+						esql1.printStackTrace();
 					}
-					
-					
-					// HACER EQUALS DE ARTICULOS 
-					if(art.getIdInterno() == -1) {
+					if(vect.size() == 0) {
 						JOptionPane.showMessageDialog(null, "EL PRODUCTO NO EXISTE");
 					}else {
-						// el producto existe
-						model.setValueAt(art.getDescripcion(), table.getSelectedRow(), 2);
+						articulo = vect.elementAt(0);
+						txtcant.requestFocus();
 					}
-					table.changeSelection(0, 1, false, false);
-					table.editCellAt(0, 1);
-					
 				}
-				//System.out.println(table.getSelectedColumn());
-				
-			}				
+			}
 		});
-		// seteo el editable en 0,1
 		
-		table.changeSelection(0, 0, false, false);
-		table.editCellAt(0, 0);
-		//table.addRowSelectionInterval(0, 0);
-		//table.addColumnSelectionInterval(0, 0);
-		//System.out.println(table.getSelectedColumn());
+		txtcod.setColumns(10);
+		txtcod.setBounds(16, 47, 103, 26);
+		contentPane.add(txtcod);
+		
+	
+		JLabel label_1 = new JLabel("CANTIDAD");
+		label_1.setBounds(124, 34, 85, 16);
+		contentPane.add(label_1);
+		
+		JButton btnEliminarProdSelec = new JButton("QUITAR PRODUCTO");
+		btnEliminarProdSelec.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int selected = table.getSelectedRow();
+				if(selected == -1) {
+					JOptionPane.showMessageDialog(null, "SELECCIONE UN PRODUCTO PARA QUITAR");
+				}else{
+					FormNuevaVenta.this.model.removeRow(selected);
+					txtcod.requestFocus();
+				}
+				
+			}
+		});
+		btnEliminarProdSelec.setForeground(Color.RED);
+		btnEliminarProdSelec.setBounds(627, 223, 150, 56);
+		contentPane.add(btnEliminarProdSelec);
 	}
-		
-		
+	
+	@Override
+	public void setVisible(boolean value) {
+	    super.setVisible(value);
+	    txtcod.requestFocusInWindow();
+	}
 }
