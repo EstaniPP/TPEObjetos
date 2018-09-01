d5e95ca75291b0fbf7f3ec2396efb25db8979c2b
Estanislao Perez Pe <Estanislao Perez Pe@LAPTOP-559QH9HJ>
2018-09-01 10:33:16 -0300

    hola2

--- Stocker/src/Forms/FormVerVenta.java
+++ Stocker/src/Forms/FormVerVenta.java
@@ -7,36 +7,23 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import Ventas.Venta;
+import javax.swing.JScrollPane;
+
 public class FormVerVenta extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					FormVerVenta frame = new FormVerVenta();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public FormVerVenta() {
+	public FormVerVenta(Venta v) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 761, 457);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 112, 719, 285);
+		contentPane.add(scrollPane);
 	}
-
 }
