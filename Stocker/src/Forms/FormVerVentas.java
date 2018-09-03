12ae72643c27f4066d40b3bee16564a91b26608d
Valentin <valentinfvazquez@gmail.com>
2018-09-03 10:02:40 -0300

    -

--- Stocker/src/Forms/FormVerVentas.java
+++ Stocker/src/Forms/FormVerVentas.java
@@ -6,10 +6,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 
 public class FormVerVentas extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -32,11 +36,17 @@ public class FormVerVentas extends JFrame {
 	 */
 	public FormVerVentas() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 569, 343);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(16, 45, 384, 241);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setColumnHeaderView(table);
 	}
-
 }
